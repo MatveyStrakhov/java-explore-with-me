126e82d3403f413b63618dafd3a5f234320ce6dc
MStrakhov <matveystrakhov@yandex.ru>
2024-04-18 19:20:31 +0300

    feat:structure

---
+++ main-service/src/main/java/ru/practicum/ewm/EWMMainServiceApp.java
@@ -0,0 +1,7 @@
+package ru.practicum.ewm;
+
+public class EWMMainServiceApp {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ stats/stats-dto/src/main/java/ru/practicum/ewm/stats/dto/EndpointHitDto.java
@@ -0,0 +1,14 @@
+package ru.practicum.ewm.stats.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class EndpointHitDto {
+    private String app;
+    private String uri;
+    private String ip;
+    private String timestamp;
+
+}
